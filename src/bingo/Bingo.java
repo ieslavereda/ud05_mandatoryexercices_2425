75de9019ab800f1f703d74d54e5f1d20f392850e
xrosilloprofe <xv.rosillo@edu.gva.es>
2024-12-17 07:59:56 +0100

    20241217 v1

--- src/bingo/Bingo.java
+++ src/bingo/Bingo.java
@@ -2,12 +2,25 @@ package bingo;
 
 public class Bingo {
     public static void main(String[] args) {
-        Juego bingo = new Juego();
+
         int numJugadores = Input.getInteger("Número de jugadores? Mínimo 2 jugadores: ");
+
         while(numJugadores<=1){
             numJugadores = Input.getInteger("Número de jugadores? Mínimo 2 jugadores: ");
         }
-        bingo.startGame(Color.VERDE,90,numJugadores);
-        bingo.startGame(Color.ROJO,50,2);
+
+        Jugador[] jugadores = new Jugador[numJugadores];
+        for (int i = 0; i < jugadores.length; i++) {
+            String nombre = Input.getString("Nombre del jugador: ");
+            jugadores[i] = new Jugador(nombre);
+
+        }
+
+        Juego bingo = new Juego(Color.VERDE,90,jugadores);
+        bingo.playGame();
+
+        bingo = new Juego(Color.ROJO,50,jugadores);
+        bingo.playGame();
+
     }
 }
--- src/bingo/Juego.java
+++ src/bingo/Juego.java
@@ -3,41 +3,52 @@ package bingo;
 public class Juego {
     private Bombo bombo;
     private Jugador[] jugadores;
+    private Color color;
+    private int numBolas;
+    
+    public Juego(Color color, int numBolas, Jugador[] jugadores){
+        this.color=color;
+        this.numBolas=numBolas;
+        this.jugadores=jugadores;
+        this.bombo = new Bombo(numBolas,color);
 
-    public void startGame(Color color, int numBolas, int numJugadores){
-        bombo = new Bombo(numBolas,color);
-        jugadores = new Jugador[numJugadores];
-        for (int i = 0; i < jugadores.length; i++) {
-            String nombre = Input.getString("Nombre del jugador: ");
-            jugadores[i] = new Jugador(nombre);
-            jugadores[i].setCarton(new Carton(numBolas));
-            System.out.println(jugadores[i]);
+        //Asignamos los cartones
+        for(Jugador jugador:jugadores){
+            jugador.setCarton(new Carton(numBolas));
+            System.out.println(jugador);
         }
-        Jugador ganador = playGame();
-        if (ganador!=null)
-            System.out.println("El ganador es: "+ganador.getNombre());
-        else
-            System.out.println("Ha habido un gran problema");
+
     }
 
-    private Jugador playGame(){
-        while(true){
+    public void playGame(){
+        Jugador[] ganadores = new Jugador[jugadores.length];
+        int posicion=0;
+
+        while(ganadores[0]==null){
             Bola bola = bombo.sacarBola();
-            if(bola==null)
-                return null;
             System.out.println("La bola sacada es: " + bola);
             System.out.println(bombo);
             for (Jugador jugador:jugadores){
                 if(jugador.getCarton().marcarCarton(bola.getNumero())==-1){
-                    return jugador;
+                    ganadores[posicion++]=jugador;
                 }
                 System.out.println(jugador);
             }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            esperar(1000);
+        }
+
+        for(Jugador ganador:ganadores){
+            if (ganador!=null)
+                System.out.println("El ganador es: "+ganador.getNombre());
+        }
+
+    }
+
+    private static void esperar(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
